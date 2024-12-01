f0e8ca5eb61718950845db95278a331339360cdc
Keith Leonardo <keith@skand.io>
2024-12-02 02:23:26 +1100

    Implement vec4 utilities

--- src/math/vec4.c
+++ src/math/vec4.c
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #include "vec4.h"
 
 vec4_t make_vec3_vec4(vec3_t a, float w) {
@@ -7,6 +9,60 @@ vec4_t make_vec3_vec4(vec3_t a, float w) {
     b.z = a.z;
     b.w = w;
     return b;
+}
+
+vec3_t make_vec4_vec3(vec4_t a) {
+    vec3_t b;
+    b.x = a.x;
+    b.y = a.y;
+    b.z = a.z;
+    return b;
+}
+
+vec4_t add_vec4(vec4_t a, vec4_t b) {
+    vec4_t c;
+    c.x = a.x + b.x;
+    c.y = a.y + b.y;
+    c.z = a.z + b.z;
+    c.w = a.w + b.w;
+    return c;
+}
+
+vec4_t sub_vec4(vec4_t a, vec4_t b) {
+    vec4_t c;
+    c.x = a.x - b.x;
+    c.y = a.y - b.y;
+    c.z = a.z - b.z;
+    c.w = a.w - b.w;
+    return c;
+}
+
+vec4_t scale_vec4(vec4_t a, float scale) {
+    vec4_t b;
+    b.x = a.x * scale;
+    b.y = a.y * scale;
+    b.z = a.z * scale;
+    b.w = a.w * scale;
+    return b;
+}
+
+vec4_t negate_vec4(vec4_t a) {
+    vec4_t b;
+    b.x = -a.x;
+    b.y = -a.y;
+    b.z = -a.z;
+    b.w = -a.w;
+    return b;
+}
+
+vec4_t normalize_vec4(vec4_t a) {
+    float length_sq = dot_vec4(a, a);
+    float inv_length = 1.0 / sqrt(length_sq);
+    return scale_vec4(a, inv_length);
+}
+
+float dot_vec4(vec4_t a, vec4_t b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
 }
 
 vec4_t apply_mat4_vec4(vec4_t a, mat4_t matrix) {
--- src/math/vec4.h
+++ src/math/vec4.h
@@ -24,6 +24,65 @@ typedef struct {
 vec4_t make_vec3_vec4(vec3_t a, float w);
 
 /**
+ * @brief Remove the w component from a 4-dimensional vector.
+ *
+ * @param a
+ * @return vec3_t
+ */
+vec3_t make_vec4_vec3(vec4_t a);
+
+/**
+ * @brief Compute the sum of vectors.
+ *
+ * @param a
+ * @param b
+ * @return vec4_t
+ */
+vec4_t add_vec4(vec4_t a, vec4_t b);
+
+/**
+ * @brief Compute the difference of vectors.
+ *
+ * @param a
+ * @param b
+ * @return vec4_t
+ */
+vec4_t sub_vec4(vec4_t a, vec4_t b);
+
+/**
+ * @brief Multiply a vector by a scalar.
+ *
+ * @param a
+ * @param scale
+ * @return vec4_t
+ */
+vec4_t scale_vec4(vec4_t a, float scale);
+
+/**
+ * @brief Negate a vector.
+ *
+ * @param a
+ */
+vec4_t negate_vec4(vec4_t a);
+
+/**
+ * @brief Normalize a vector.
+ *
+ * @param a
+ * @return vec4_t
+ */
+vec4_t normalize_vec4(vec4_t a);
+
+/**
+ * @brief Compute the dot product of vectors.
+ *
+ * @param a
+ * @param b
+ * @return float
+ */
+float dot_vec4(vec4_t a, vec4_t b);
+
+/**
  * @brief Apply a 4x4 matrix to a vector.
  *
  * @param a
